f80ecf577692d138e0cf169c020a7ec1a605d0b0
prakashvarma <sprakashvarma@gmail.com>
2022-03-20 20:55:13 +0530

    Test case

--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,9 +1,35 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.execution.ExecutionService;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+
+@RunWith(MockitoJUnitRunner.class)
 public class TradingStrategyTest {
+
+    private String stock;
+
+    private double limit;
+
+    @Mock
+    ExecutionService service;
+
+    @Before
+    public void setUp(){
+        this.stock = "ACME:EUR";
+        this.limit = 54.0;
+    }
+
     @Test
-    public void testNothing() {
+    public void testPriceUpdate(){
+        TradingStrategy tradingStrategy = new TradingStrategy(stock, limit, service);
+        verify(tradingStrategy, times(1)).priceUpdate(stock, limit);
     }
 }
